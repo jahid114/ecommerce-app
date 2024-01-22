e81fc44abaee7cb217427f289ea91d4ffa330131
MD Zanea Alam Jahid <jahidctgcse@gmail.com>
2024-01-22 14:00:12 +0600

    feat: adds Product Entity

---
+++ src/main/java/com/jahid/ecommerce/api/product/Product.java
@@ -0,0 +1,50 @@
+package com.jahid.ecommerce.api.product;
+
+import com.fasterxml.jackson.databind.annotation.EnumNaming;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+enum Category{
+    UNKNOWN,
+    SMARTPHONE,
+    ELECTRONICS,
+    MOBILE_ACCESSORIES,
+    COMPUTER_ACCESSORIES
+}
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id", nullable = false)
+    private Long product_id;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "product_details")
+    private String productDetails;
+
+    @Column(name = "product_image_url")
+    private String productImageUrl;
+
+    @Column(name = "price",nullable = false)
+    private int price;
+
+    @Column(name = "in_stock",nullable = false)
+    private int inStock;
+
+    @Column(name = "sku", nullable = false)
+    private String sku;
+
+    @Column(name = "product_category",columnDefinition = ("varchar(255) default 'UNKNOWN'"))
+    @Enumerated(EnumType.STRING)
+    private Category productCategory;
+}
