e4c4e0579f091404de87fd90ef4781d3363f9a7e
jahid114 <jahidctgcse@gmail.com>
2024-01-25 22:50:15 +0600

    fix: adds createCartItem  - adds createCartItem in CartItemController  - adds createCartItem in CartItemService

--- src/main/java/com/jahid/ecommerce/api/cart_item/CartItemController.java
+++ src/main/java/com/jahid/ecommerce/api/cart_item/CartItemController.java
@@ -1,8 +1,12 @@
 package com.jahid.ecommerce.api.cart_item;
 
 import com.jahid.ecommerce.api.cart.CreateCartDto;
+import com.jahid.ecommerce.api.product.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +20,8 @@ public class CartItemController {
     }
 
     @PostMapping
-    public CreateCartDto createCartItem(CreateCartDto createCartDto){
-        return createCartDto;
+    public ResponseEntity<ResponseCartItemDto> createCartItem(@RequestBody CreateCartItemDto createCartItemDto){
+        ResponseCartItemDto cartItem = cartItemService.createCartItem(createCartItemDto);
+        return new ResponseEntity<>(cartItem, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/jahid/ecommerce/api/cart_item/CartItemService.java
+++ src/main/java/com/jahid/ecommerce/api/cart_item/CartItemService.java
@@ -1,5 +1,10 @@
 package com.jahid.ecommerce.api.cart_item;
 
+import com.jahid.ecommerce.api.cart.Cart;
+import com.jahid.ecommerce.api.cart.CartRepository;
+import com.jahid.ecommerce.api.product.Product;
+import com.jahid.ecommerce.api.product.ProductRepository;
+import com.jahid.ecommerce.api.utility.NotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,9 +13,28 @@ import org.springframework.stereotype.Service;
 public class CartItemService {
     private final CartItemRepository cartItemRepository;
     private final ModelMapper modelMapper;
+    private final ProductRepository productRepository;
+    private final CartRepository cartRepository;
 
-    CartItemService(@Autowired CartItemRepository cartItemRepository, @Autowired ModelMapper modelMapper){
+    CartItemService(@Autowired CartItemRepository cartItemRepository, @Autowired ModelMapper modelMapper, @Autowired ProductRepository productRepository,
+                    CartRepository cartRepository){
         this.cartItemRepository = cartItemRepository;
         this.modelMapper = modelMapper;
+        this.productRepository = productRepository;
+        this.cartRepository = cartRepository;
+    }
+
+    public ResponseCartItemDto createCartItem(CreateCartItemDto createCartItemDto) {
+        Product product = productRepository.findById(createCartItemDto.getProductId()).orElseThrow(()->new NotFoundException(createCartItemDto.getProductId(),Product.class.getName()));
+        Cart cart = cartRepository.findById(createCartItemDto.getCartId()).orElseThrow(()->new NotFoundException(createCartItemDto.getCartId(),Cart.class.getSimpleName()));
+        CartItem cartItem = this.modelMapper.map(createCartItemDto, CartItem.class);
+        cartItem.setUnitPrice(product.getPrice());
+        cartItem.setTotalPrice(((long) product.getPrice() * cartItem.getItemQuantity()));
+//        cart.getCartItemSet().add(cartItem);
+//        product.getCartItemSet().add(cartItem);
+        cartItem.setCart(cart);
+        cartItem.setProduct(product);
+        CartItem createdItem = cartItemRepository.save(cartItem);
+        return modelMapper.map(createdItem, ResponseCartItemDto.class);
     }
 }
