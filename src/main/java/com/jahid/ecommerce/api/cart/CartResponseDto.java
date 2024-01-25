d079f0e3d68a55763d544f133063e7783a918a19
jahid114 <jahidctgcse@gmail.com>
2024-01-25 22:48:55 +0600

    fix: modifies CartResponseDto to show cartItem

--- src/main/java/com/jahid/ecommerce/api/cart/CartResponseDto.java
+++ src/main/java/com/jahid/ecommerce/api/cart/CartResponseDto.java
@@ -1,10 +1,13 @@
 package com.jahid.ecommerce.api.cart;
 
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Set;
+
 
 /**
  * DTO for {@link Cart}
@@ -17,6 +20,8 @@ public class CartResponseDto implements Serializable {
     private Long totalPrice;
     private int totalQuantity;
     private UserDto user;
+    private Set<ResponseCartItemDto> cartItemSet;
+
 
     @NoArgsConstructor
     @Getter
@@ -28,5 +33,15 @@ public class CartResponseDto implements Serializable {
         private boolean isActive;
         private String email;
         private String address;
+    }
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    private static class ResponseCartItemDto implements Serializable {
+        private Long cartItemId;
+        private int unitPrice;
+        private Long totalPrice;
+        private int itemQuantity;
+        private com.jahid.ecommerce.api.cart_item.ResponseCartItemDto.ProductDto product;
     }
 }
