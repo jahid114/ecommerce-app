962904cea63cbeb077ee8431b955d3a10c47b49e
jahid114 <jahidctgcse@gmail.com>
2024-07-27 01:12:29 +0600

    Refactor: re-organized ProductResponse

--- src/main/java/com/ecommerce/api/product/response/ProductResponse.java
+++ src/main/java/com/ecommerce/api/product/response/ProductResponse.java
@@ -1,25 +1,19 @@
 package com.ecommerce.api.product.response;
 
-import com.ecommerce.api.product.model.Product;
 import com.ecommerce.api.utility.EnumConstants;
 import lombok.*;
 
-import java.io.Serializable;
-
-/**
- * DTO for {@link Product}
- */
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class ProductResponse implements Serializable {
-    private Long productId;
+public class ProductResponse {
+    private Long id;
+    private Integer price;
+    private Integer inStock;
+    private String sku;
     private String productName;
     private String productDetails;
-    private int price;
-    private int inStock;
-    private String sku;
-    private EnumConstants.Category productCategory;
     private String productImageUrl;
     private String productImagePath;
+    private EnumConstants.Category productCategory;
 }
